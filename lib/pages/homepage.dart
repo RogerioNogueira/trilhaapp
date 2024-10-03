2c2dc16307a302f2a6df9fa28b908d93e0259509
RogerioNogueira <rogerio.nogueira.dell@gmail.com>
2024-10-02 23:34:14 -0300

    go

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,10 +1,8 @@
 
 
-//import 'package:google_fonts/google_fonts.dart';
 import 'package:trilhaapp/pages/login.dart';
-//import 'package:trilhaapp/services/gerador_numero_aleatorio.dart';
 import 'package:flutter/material.dart';
-//import 'package:google_fonts/google_fonts.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -22,61 +20,3 @@ class _HomePageState extends State<HomePage> {
     return const LoginPage(); 
   }
 }
-
-
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         //backgroundColor: Colors.blueAccent ,
-//         title: Text(style: GoogleFonts.aBeeZee(), 'Meu App'),
-//       ),
-//       body: Container(
-//         width: double.infinity,
-//         margin: const EdgeInsets.symmetric(horizontal: 16,vertical: 8), 
-
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.center,  //Eixo horizontal 
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Text(style: GoogleFonts.arima(fontSize: 50,color: Colors.red),'O número gerado foi: $numero'),
-//             Text(style: GoogleFonts.arima(), 'O botão foi pressionado  $quantidadeCliques vezes' ),
-           
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               children: [
-//                 Expanded(
-//                   child: Container(
-//                     margin: EdgeInsets.all(10),
-//                     color: Colors.amber,
-//                     child: Text('X1')
-//                   ),
-//                 ),
-//                 Expanded(
-//                   child: Container(
-//                     margin: EdgeInsets.all(10),
-//                     color: Colors.redAccent,
-//                     child: Text('X2')
-//                   ),
-//                 ),
-//                 Expanded(
-//                   child: Container(
-//                     margin: EdgeInsets.all(10),
-//                     color: Colors.blueAccent,
-//                     child: Text('X3')
-//                   ),
-//                 ),
-//               ],)
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//           child: const Icon(Icons.recycling),
-//           onPressed: () {
-//             setState(() {
-//               quantidadeCliques+=1;
-//               numero = GeradorDeNumeroAleatorio.gerarNumeroAleatorio(20);
-//             });
-//           }),
-//     );
-//   }
-// }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -12,70 +12,144 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       
-      backgroundColor: Colors.lightBlueAccent ,
-      body: SizedBox(
-        
-        width: double.infinity,
-        child: Column(
-             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-             const  SizedBox(
-                height: 70,
-              ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(  //Scrool que limita o teclado
+        child: ConstrainedBox(
+          constraints:
+              BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
+              //a box obtem o tamanho total dispositivo que esta executando
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 70,
+                ),
+                   
                
-               Container(
-                child: Icon(Icons.person_2,size:125,color: Colors.red, ),
+                Row(
+                  children: [
+                    Expanded(child: Container()),
+                    Expanded(
+                      flex: 2,
+                      child: Image.network(
+                        "https://logopng.com.br/logos/google-37.png",
+                      ),
+                    ),
+                    Expanded(child: Container()),
+                  ],
+                ),
+                
+                const SizedBox(
+                  height: 30,
+                ),
+                const Center(
+                    child: Text(
+                  'Já tem cadastro ?',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                )),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Center(
+                    child: Text('Primeiro faça o login, depois a diferença.',
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.normal))),
               
+                const SizedBox(
+                  height: 50,
+                ),
+          
+                Container(
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  margin: const EdgeInsetsDirectional.symmetric(horizontal: 30),
+                  height: 50,
+                  child: const TextField(
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.only(top: 5),
+                        hintText: 'informe seu email ',
+                        icon: Icon(Icons.email),
+                        hintStyle: TextStyle(color: Colors.grey)),
+                  ),
+                ),
+                
+          
+          
+                Container(
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  margin: const EdgeInsetsDirectional.symmetric(horizontal: 30),
+                  height: 50,
+                  child: const TextField(
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.only(top: 15),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black)),
+                        hintText: 'Senha ',
+                        icon: Icon(
+                            Icons.lock), //icone  desgrudadoe do edit   $ ___
+                        suffixIcon: Icon(Icons.visibility),
+                        hintStyle: TextStyle(color: Colors.grey)),
+                  ),
+                ),
+          
+                
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  margin: const EdgeInsetsDirectional.symmetric(horizontal: 30),
+                  height: 50,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                  5))), //serve para arrredondar as bordas
+                          backgroundColor: WidgetStateProperty.all(
+                              Colors.lightBlue)), //dar cor ao botao
+                      child: const Text('ENTRAR',
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white)),
+                    ),
+                  ),
+                              
+                ),
+                Expanded(child: Container()),
+                Container(
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  margin: const EdgeInsetsDirectional.symmetric(horizontal: 30),
+                  height: 50,
+                  child: const Text('Cadastro',
+                      style: TextStyle(
+                        fontSize: 14,
+                      )),
+                ),
+                
+                Container(
+                    alignment: Alignment.center,
+                    width: double.infinity,
+                    margin:
+                        const EdgeInsetsDirectional.symmetric(horizontal: 30),
+                    height: 50,
+                    child: const Text('Criar conta',
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue))),
+                const Padding(padding: EdgeInsets.all(30)),
+              ],
             ),
-
-            const SizedBox(height: 50,),
-
-   
-            Container(              
-              alignment: Alignment.center,
-              width: double.infinity,
-              margin:const  EdgeInsetsDirectional.symmetric(horizontal: 30),
-              color: Colors.blueGrey,
-              height: 50,
-              child: const Text('informe o email '),
-            ),
-            
-            const SizedBox(height: 10,),
-
-            Container(
-              alignment: Alignment.center,
-              width: double.infinity,
-              margin:const  EdgeInsetsDirectional.symmetric(horizontal: 30),
-              color: Colors.blueGrey,
-              height: 50,
-              child: const Text('informe a senha'),
-            ),
-
-            
-            Expanded(child: Container(),),
-            
-            Container(
-              alignment: Alignment.center,
-              width: double.infinity,
-              margin:const  EdgeInsetsDirectional.symmetric(horizontal: 30),
-              color: Colors.blueGrey,
-              height: 50,
-              child: const Text('Botão'),
-            ),
-            
-             const SizedBox(height: 10,),
-            
-            Container(
-              alignment: Alignment.center,
-              width: double.infinity,
-              margin:const  EdgeInsetsDirectional.symmetric(horizontal: 30),
-              color: Colors.blueGrey,
-              height: 50,
-              child: const Text('Botão'),
-            ),
-            
-            const Padding(padding: EdgeInsets.all(10)),            
-          ],
+          ),
         ),
       ),
     );
